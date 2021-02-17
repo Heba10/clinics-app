364a2941880896d99a281c587dfa20c33f392af7
Heba10 <hebe10@yahoo.com>
2021-02-17 23:10:15 +0200

    adding style

--- resources/views/clinics/index.blade.php
+++ resources/views/clinics/index.blade.php
@@ -5,14 +5,17 @@
 <div class="container m-5">
 @foreach($clinics as $clinic)
 <div class="card">
+
   <div class="card-header">
-  {{ $clinic->name }}
+  <h5>
+  {{ $clinic->name }} 
+  </h5>
   </div>
   <div class="card-body">
     <p class="card-text">{{ $clinic->description }}</p>
 
-    <h5 class="card-title">Special title treatment <a href="{{ $clinic->website }}" >{{ $clinic->website }}</a></h5>
-    <a href="{{route('clinics.show',['clinic' => $clinic->id])}}"  class="btn btn-primary">View Details</a>
+    <h5 class="card-title"><a href="{{ $clinic->website }}" >{{ $clinic->website }}</a> الموقع الالكتروني </h5>
+    <a href="{{route('clinics.show',['clinic' => $clinic->id])}}"  class="btn btn-primary">المزيد</a>
   </div>
 </div>
 @endforeach
--- resources/views/clinics/show.blade.php
+++ resources/views/clinics/show.blade.php
@@ -13,23 +13,39 @@
 <div class="container m-5">
 <div class="card">
 <div class="card-header">
-  {{ $clinic->name }}
+<h5>
+  {{ $clinic->name }}</h5>
   </div>
   <div class="card-body">
-    <h5 class="card-title">{{$clinic->name}}</h5>
     <p class="card-text">{{$clinic->description}}</p>
-    <h5 class="card-title">Special title treatment <a href="{{ $clinic->website }}" >{{ $clinic->website }}</a></h5>
+    <h5 class="card-title"><a href="{{ $clinic->website }}" >{{ $clinic->website }}</a> الموقع الالكتروني </h5>
    
   </div>
 </div>
 
 <div class="form-group">
-      <label for="exampleInputPassword1">branchs</label>
-    
-        @foreach($branchs as $branch)  
-          <option value="{{$branch->id}}">{{$branch->phone}}</option>
-          <option value="{{$branch->id}}">{{$branch->address}}</option>
+      <label for="exampleInputPassword1" class="m-5"><h5>الفروع</h5></label>
+
+      <table class="table" lang="ar" dir="rtl">
+
+  
+  <tbody>
+   
+     
+      
+      @foreach($branchs as $branch)  
+      <tr>
+      <td>{{$branch->address}}</td>
+      <td>ت/ {{$branch->phone}}</td>
+      
+      </tr>
         @endforeach
+    
+   
+  </tbody>
+</table>
+    
+       
        
     </div>
 </div>
